31121d03ebe19483fa54ccc941ead3073be1695f
Luma Montes <lumagoesmontes@gmail.com>
2022-07-03 13:44:12 -0300

    adicionado logica de inscricao e pagamento no stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,7 @@
 
+import { useSession, signIn } from 'next-auth/react'
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -6,11 +9,32 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
+    
+    const {data: sesssion} = useSession();
 
+    async function handleSubscribe(){
+        if(!sesssion){
+            signIn('github');
+            return;
+        }
+
+        // #Criação da checkout session
+        try {
+            const response = await api.post('/subscribe');
+            const { sessionId } = response.data;
+            const stripe = await getStripeJs();
+
+            await stripe.redirectToCheckout({sessionId});
+        } catch (err) {
+            alert(err.message);
+        }
+
+    }
     return (
         <button
             className={styles.subscribeButton}
             type='button'
+            onClick={handleSubscribe}
         >
             Subscribe now
         </button>
